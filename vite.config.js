779780c0a506c79f8058e2f069da6d4f2e0b5d96
Govil-web <gvillegas1276@hotmail.com>
2025-11-27 21:26:16 -0500

    Revert to GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/globalia-front/',
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
